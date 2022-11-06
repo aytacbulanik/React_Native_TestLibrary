2e529ed879ca757738f67cc26fb3e47b60a29ddb
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-06 15:40:01 +0300

    new library is useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import {Text, SafeAreaView, StyleSheet, FlatList} from 'react-native';
+import {Text, SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
-import NewsCard from './components/NewsCard';
-import newsData from './news_data.json';
 
 const App = () => {
-  const renderNews = ({item}) => <NewsCard news={item} />;
-
+  
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.large_title}> News </Text>
-      <FlatList data={newsData} renderItem={renderNews} />
+      <Text> Deneme</Text>
     </SafeAreaView>
   );
 };
