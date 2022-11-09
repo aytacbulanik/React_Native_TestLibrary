17d6407394ffc0e3c257a2f8ac05b9163d881ea0
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-09 23:05:07 +0300

    moving between screens

--- src/pages/First.js
+++ src/pages/First.js
@@ -1,12 +1,27 @@
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, StyleSheet, Button} from 'react-native';
 import React from 'react';
 
-const First = () => {
+const First = props => {
+  console.log(props);
+  function gotoSecondPage() {
+    props.navigation.navigate('SecondScreen');
+  }
   return (
-    <SafeAreaView>
-      <Text>First !!!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.textDetail}>First !!!</Text>
+      <Button title="GİT" onPress={gotoSecondPage} />
     </SafeAreaView>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textDetail: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
 export default First;
--- src/pages/Second.js
+++ src/pages/Second.js
@@ -1,10 +1,14 @@
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, Button} from 'react-native';
 import React from 'react';
 
-const second = () => {
+const second = props => {
+  function backFunc() {
+    props.navigation.goBack();
+  }
   return (
     <SafeAreaView>
       <Text>second</Text>
+      <Button title="Go First" onPress={backFunc} />
     </SafeAreaView>
   );
 };
