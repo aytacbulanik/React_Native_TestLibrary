9e6bc8c41549f6252737fa8a6782859b33082fac
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-14 18:58:14 +0300

    data is showing with flatlist

--- src/pages/Favorite/Favorite.js
+++ src/pages/Favorite/Favorite.js
@@ -1,20 +1,27 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, FlatList} from 'react-native';
+import React, {useState} from 'react';
 import Button from '../../Components/Button';
 import axios from 'axios';
 
 const Favorite = ({navigation}) => {
+  const [cities, setCites] = useState([]);
   function request() {
     axios
       .get('https://jsonplaceholder.typicode.com/users')
-      .then(object => {
-        console.log(object.data[0].address.city);
-      })
+      .then(object =>
+        setCites(
+          object.data.map(data => {
+            return data.address.city;
+          }),
+        ),
+      )
       .catch(error => console.log(error));
   }
+  console.log(cities);
   return (
     <View>
       <Text>Favorite</Text>
+      <FlatList data={cities} renderItem={isim => <Text>{isim.item}</Text>} />
       <Button text="GoToEdit" onPress={request} />
     </View>
   );
