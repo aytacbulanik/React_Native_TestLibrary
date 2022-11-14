b94553a6b2e92ff184664430b7cddc753f3917fb
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-14 18:17:28 +0300

    data solwed with axios

--- src/pages/Favorite/Favorite.js
+++ src/pages/Favorite/Favorite.js
@@ -1,15 +1,21 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import Button from '../../Components/Button';
+import axios from 'axios';
 
 const Favorite = ({navigation}) => {
+  function request() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(object => {
+        console.log(object.data[0].address.city);
+      })
+      .catch(error => console.log(error));
+  }
   return (
     <View>
       <Text>Favorite</Text>
-      <Button
-        text="GoToEdit"
-        onPress={() => navigation.navigate('FavoriteEditScreen')}
-      />
+      <Button text="GoToEdit" onPress={request} />
     </View>
   );
 };
