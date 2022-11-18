7ce3acc0fda0f039f6a7e820bc7900e3743b67e4
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-18 16:00:44 +0300

    Error and Loading components are created

---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import LottieView from 'lottie-react-native';
+
+function Loading() {
+  return <LottieView source={require('../../assest/loading.json')} />;
+}
+
+export default Loading;
