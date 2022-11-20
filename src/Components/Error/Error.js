acd86464062e4c569e5f80882f6fc7633b58bd48
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-20 14:59:10 +0300

    store app will write again

--- src/Components/Error/Error.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LottieView from 'lottie-react-native';
-
-function Error() {
-  return <LottieView source={require('../../assest/error.json')} autoPlay />;
-}
-
-export default Error;
