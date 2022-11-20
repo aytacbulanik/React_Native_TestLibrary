dd73699e391c07757d9536bc53113014cfac1741
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-20 15:30:32 +0300

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import {SafeAreaView, Text} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import HomeScreen from './Pages/HomeScreen/HomeScreen';
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>App</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
