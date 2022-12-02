73f2759b35f89bd5697e9fefd93fb19eac565839
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-12-02 16:08:54 +0300

    files deleted for Formik

--- src/Pages/HomeScreen/HomeScreen.js
+++ src/Pages/HomeScreen/HomeScreen.js
@@ -1,28 +1,10 @@
-import {View, FlatList} from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
-import CategoryLabel from '../../components/CategoryLabel/CategoryLabel';
 
 const HomeScreen = ({navigation}) => {
-  const gelenData = [
-    {id: 0, name: 'electronics'},
-    {id: 1, name: 'jewelery'},
-    {id: 2, name: 'mens clothing'},
-    {id: 3, name: 'womens clothing'},
-  ];
-
-  const gotoProductScreen = id => {
-    navigation.navigate('ProductScreen', {id});
-  };
-
-  const categoryRenderItem = ({item}) => (
-    <CategoryLabel
-      category={item}
-      onSelect={() => gotoProductScreen(item.id)}
-    />
-  );
   return (
     <View>
-      <FlatList data={gelenData} renderItem={categoryRenderItem} />
+      <Text>Deneme</Text>
     </View>
   );
 };
