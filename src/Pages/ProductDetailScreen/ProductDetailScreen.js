c739e1405382dc4c33560bb9ea197dc00ae05bb9
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-12-02 16:15:20 +0300

    formik add project

--- src/Pages/ProductDetailScreen/ProductDetailScreen.js
+++ src/Pages/ProductDetailScreen/ProductDetailScreen.js
@@ -1,21 +1,11 @@
 import {View, Text, Image} from 'react-native';
 import React from 'react';
-import FetchData from '../../Hooks/FetchData';
 import styles from './ProductDetailScreen.Style';
 
 const ProductDetailScreen = ({route}) => {
-  const {id} = route.params;
-  const {data, loading, error} = FetchData(
-    'https://fakestoreapi.com/products/' + id,
-  );
-
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{uri: data.image}} />
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>{data.title}</Text>
-        <Text style={styles.price}>{data.price} TL</Text>
-      </View>
+      <Text> deneme</Text>
     </View>
   );
 };
--- src/Pages/ProductScreen/ProductScreen.js
+++ src/Pages/ProductScreen/ProductScreen.js
@@ -1,42 +1,10 @@
-import {SafeAreaView, FlatList} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
 import React from 'react';
 
-import Product from '../../components/Product/Product';
-import FetchData from '../../Hooks/FetchData';
-
-const ProductScreen = ({navigation, route}) => {
-  const {data, loading, error} = FetchData('https://fakestoreapi.com/products');
-  const goToDetail = id => {
-    navigation.navigate('ProductDetailScreen', {id});
-  };
-  const {id} = route.params;
-  let categoryName = '';
-  switch (id) {
-    case 0:
-      categoryName = 'electronics';
-      break;
-    case 1:
-      categoryName = 'jewelery';
-      break;
-    case 2:
-      categoryName = "men's clothing";
-      break;
-    case 3:
-      categoryName = "women's clothing";
-      break;
-  }
-  const filteredData = data.filter(item => item.category === categoryName);
-  const renderProduct = ({item}) => (
-    <Product product={item} onSelect={() => goToDetail(item.id)} />
-  );
-
+const ProductScreen = () => {
   return (
     <SafeAreaView>
-      <FlatList
-        data={filteredData}
-        renderItem={renderProduct}
-        numColumns={'2'}
-      />
+      <Text>Deneme</Text>
     </SafeAreaView>
   );
 };
